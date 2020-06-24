fa5da45d02cc0d64363a088498c11ed9599f7752
kamoto0427 <takahiro.08039@gmail.com>
2020-06-24 21:45:44 +0900

    saiko

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     @posts = Post.all
   end
@@ -14,18 +15,33 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @post.save
-    redirect_to post_path(@post)
+    if @post.save
+    redirect_to post_path(@post), notice: '投稿されました'
+    else
+      render :new, '投稿できませんでした'
+    end
   end
 
   def edit
     @post = Post.find(params[:id])
+    if @post.user != current_user
+      redirect_to posts_path, alert: '不正なアクセスです'
+    end
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+    redirect_to post_path(@post), notice: '投稿が更新されました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
   private
