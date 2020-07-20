7a039d59244f9f27925c5fb123149ecb1a600d54
kamoto0427 <takahiro.08039@gmail.com>
2020-07-20 22:47:40 +0900

    create-comments-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :comments
 
   def already_favorited?(post)
     self.favorites.exists?(post_id: post.id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200629074201) do
+ActiveRecord::Schema.define(version: 20200720134147) do
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
