631a7c38a681a8ebae96486b6aea7d47341cd29f
kamoto0427 <takahiro.08039@gmail.com>
2020-08-23 23:32:06 +0900

    gem seed-fuの導入
    seeds.rbにカテゴリーデータを記述していたが、seed-fuを導入し、カテゴリー機能実装において効率的に実装できるようにする。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,21 @@
-snowboard = Category.create(name: "スノーボード")
-snowboard.children.create([
-{name: "BURTON"},
-{name: "K2"},
-{name: "SALOMON"}
-])
+# snowboard = Category.create(name: "スノーボード")
+# snowboard.children.create([
+# {name: "BURTON"},
+# {name: "K2"},
+# {name: "SALOMON"},
+# {name: "OGASAKA"},
+# {name: "RIDE"},
+# {name: "FLOW"},
+# {name: "FLOW"},
+# {name: "FLOW"},
+# {name: "FLOW"},
+# {name: "FLOW"},
 
-snowboots = Category.create(name: "ブーツ")
-snowboots.children.create([
-{name: "BURTON"},
-{name: "DEELUXE"},
-{name: "FLUX"}
-])
+# ])
+
+# snowboots = Category.create(name: "ブーツ")
+# snowboots.children.create([
+# {name: "BURTON"},
+# {name: "DEELUXE"},
+# {name: "FLUX"}
+# ])
