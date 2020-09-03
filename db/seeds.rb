0c221366171cf55966eae3e52103924327de2e3c
kamoto0427 <takahiro.08039@gmail.com>
2020-09-03 21:16:28 +0900

    category_idにデータ取得できた

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
 snowboard = Category.create(name: "スノーボード")
 snowboard_1 = snowboard.children.create([
   {name: "BURTON"},
-  {name: "K2"},
-  {name: "SALOMON"}])
+  {name: "K2"}])
 
 snowboots = Category.create(name: "ブーツ")
 snowboots_1 = snowboots.children.create([
   {name: "BURTON"},
-  {name: "DEELUXE"},
-  {name: "FLUX"}])
+  {name: "DEELUXE"}])
