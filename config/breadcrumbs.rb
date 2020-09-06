16fc7b08d72b30279f956816fa19b0439a7c5386
kamoto0427 <takahiro.08039@gmail.com>
2020-09-06 17:49:39 +0900

    user関連のパンくず機能

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,5 +1,19 @@
 crumb :root do
-  link "Home", root_path
+  link "ホーム画面", root_path
+end
+
+crumb :users do
+  link "snowBユーザー", users_path, method: :get
+end
+
+crumb :mypage do
+  link "#{current_user.name}のマイページ", user_path, method: :get
+  parent :users
+end
+
+crumb :mypage_edit do
+  link "マイページの編集", edit_user_path
+  parent :mypage
 end
 
 # crumb :projects do
