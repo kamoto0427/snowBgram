14e9b935d30bf783624833efb021481451673916
kamoto0427 <takahiro.08039@gmail.com>
2020-09-06 18:11:02 +0900

    投稿関連のパンくず機能

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -15,3 +15,21 @@ crumb :mypage_edit do
   link "マイページの編集", edit_user_path
   parent :mypage
 end
+
+crumb :posts do
+  link "snowBの一覧ページ", posts_path, method: :get
+end
+
+crumb :post_new do
+  link "snowBの新規投稿", new_post_path, method: :get
+end
+
+crumb :post_show do
+  link "投稿の詳細ページ", post_path, method: :get
+  parent :posts
+end
+
+crumb :post_edit do
+  link "投稿の編集ページ", edit_post_path
+  parent :post_show
+end
