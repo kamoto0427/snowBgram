c9d0f3aabbc5c3d73358d7ac3e014785be9921c9
kamoto0427 <takahiro.08039@gmail.com>
2020-09-26 21:40:24 +0900

    email,password,passwordの確認テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,5 +14,23 @@ describe User do
       expect(user.errors[:name]).to include("を入力してください")
     end
 
+    it "emailがない場合はユーザー登録できない" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "passwordがない場合はユーザー登録できない" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合はユーザー登録できない" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
   end
 end
