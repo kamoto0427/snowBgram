53a8b30cdfb43a7600bc14418fd21dd44f669c86
kamoto0427 <takahiro.08039@gmail.com>
2020-09-26 21:34:38 +0900

    単体テスト通過テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
-    # it "name,email,パスワード,パスワードの確認が全て存在すればユーザー登録できる" do
-    #   user = build(:user)
-    #   expect(user).to be_valid
-    # end
+    it "name,email,パスワード,パスワードの確認が全て存在すればユーザー登録できる" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-    # it "nameがない場合はユーザー登録できない" do
-    #   user = build(:user, name: nil)
-    #   user.valid?
-    #   expect(user.errors[:name]).to include("を入力してください")
-    # end
+    it "nameがない場合はユーザー登録できない" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
 
   end
 end
