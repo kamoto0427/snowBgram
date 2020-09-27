723cd0ffa85c5c181b193ff3a1a2c89fbe8df7d4
kamoto0427 <takahiro.08039@gmail.com>
2020-09-27 14:36:33 +0900

    userモデルに英数字バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,12 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX}, length: { minimum: 8 }
-
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{8,100}\z/
+  validates :password, presence: true,
+             format: { with: VALID_PASSWORD_REGEX,
+              message: "は半角8文字以上の英大文字・小文字・数字それぞれ１文字以上含む必要があります ex.Aa000000"},
+             length: { minimum: 8 }
+             
   attachment :profile_image
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
