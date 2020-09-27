7e19a3664dccefa673e3a1ee77b5ae746b401d2d
kamoto0427 <takahiro.08039@gmail.com>
2020-09-27 23:13:14 +0900

    投稿テストの大枠完成

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,6 +7,24 @@ describe Post do
       post = build(:post)
       expect(post).to be_valid
     end
-    
+
+    it "titleがない場合は投稿できない" do
+      post = build(:post, title: nil)
+      post.valid?
+      expect(post.errors[:title]).to include("を入力してください")
+    end
+
+    it "appealがない場合は投稿できない" do
+      post = build(:post, appeal: nil)
+      post.valid?
+      expect(post.errors[:appeal]).to include("を入力してください")
+    end
+
+    it "imageがない場合は投稿できない" do
+      post = build(:post, image: nil)
+      post.valid?
+      expect(post.errors[:image]).to include("を入力してください")
+    end
+
   end
 end
