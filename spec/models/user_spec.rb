5a14d5193551187fb19963b5dbb973b2240fb09a
kamoto0427 <takahiro.08039@gmail.com>
2020-09-27 14:49:22 +0900

    英数字の単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,14 +40,32 @@ describe User do
     end
 
     it "passwordが8文字以上であればユーザー登録できる" do
-      user = build(:user, password: "aaaa0000", password_confirmation: "aaaa0000")
+      user = build(:user, password: "Aaaa1234", password_confirmation: "Aaaa1234")
       expect(user).to be_valid
     end
 
     it "passwordが7文字以下であればユーザー登録できない" do
-      user = build(:user, password: "aaa0000", password_confirmation: "aaa0000")
+      user = build(:user, password: "Aaa1234", password_confirmation: "Aaa1234")
       user.valid?
       expect(user.errors[:password]).to include
+    end
+
+    it "パスワードに英小文字が含まれない場合は無効" do
+      user = build(:user, password: '1'+'A' * 5, password_confirmation: '1A'+'a' * 3)
+      user.valid?
+      expect(user.errors[:password]).to include('は半角8文字以上の英大文字・小文字・数字それぞれ１文字以上含む必要があります ex.Aa000000')
+    end
+
+    it "パスワードに英大文字が含まれない場合は無効" do
+      user = build(:user, password: '1'+'a' * 5, password_confirmation: '1A'+'a' * 3)
+      user.valid?
+      expect(user.errors[:password]).to include('は半角8文字以上の英大文字・小文字・数字それぞれ１文字以上含む必要があります ex.Aa000000')
+    end
+
+    it "パスワードに数字が含まれない場合は無効" do
+      user = build(:user, password: 'A'+'a' * 5, password_confirmation: '1A'+'a' * 3)
+      user.valid?
+      expect(user.errors[:password]).to include('は半角8文字以上の英大文字・小文字・数字それぞれ１文字以上含む必要があります ex.Aa000000')
     end
 
   end
