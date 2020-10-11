1e6002955cea63ee3125445336ca5371d2ae1539
kamoto0427 <takahiro.08039@gmail.com>
2020-10-11 18:02:27 +0900

    newアクションのテスト成功

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
-describe PostsController do
+describe PostsController, type: :controller do
+
+  describe 'GET #new' do
+    it "new.html.erbに遷移すること" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
 end
