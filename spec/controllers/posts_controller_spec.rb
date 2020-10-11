994c4fbe6515f14487064d51f41d1c49673f8b7b
kamoto0427 <takahiro.08039@gmail.com>
2020-10-11 18:16:51 +0900

    editの通過

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,9 +3,23 @@ require 'rails_helper'
 describe PostsController, type: :controller do
 
   describe 'GET #new' do
-    it "new.html.erbに遷移すること" do
+    it "new.html.hamlに遷移すること" do
       get :new
       expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it "@postに正しい値が入っていること" do
+      post = create(:post)
+      get :edit, params: { id: post }
+      expect(assigns(:post)).to eq post
+    end
+
+    it "edit.html.hamlに遷移すること" do
+      post = create(:post)
+      get :edit, params: { id: post }
+      expect(response).to render_template :edit
     end
   end
 end
